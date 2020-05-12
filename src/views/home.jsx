fc1b29b0f826efcfbd633a7e25a64f07bd2a0437
qinqiang <qinqiang@shoudaozi.com>
2020-05-12 13:42:43 +0800

    axios + reducer 使用

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -3,32 +3,43 @@ import { connect } from 'react-redux';
 import {setCount,} from '../redux/actions'
 import styles from './home.module.scss';
 import { Button } from 'zent';
+import {login} from '../api/user';
 
-class ReduxTest extends Component {
+class Home extends Component {
+  constructor (props){
+    super(props);
+    this.state = {
+
+    }
+  }
   componentDidMount() {
     console.log(this.props)
+  }
+  doLogin = () =>{
+    login();
   }
   render() {
     const { PayIncrease,tiger } = this.props;
     return (
       <div className={`${styles.test}`}>
-        <h2>当月工资为{tiger}</h2>
-        <Button type="success" onClick={PayIncrease}>升职加薪</Button>
+        <h2>reducer:{tiger}</h2>
+        <Button type="success"  onClick={PayIncrease}>dispatch</Button>
+        <Button onClick={ this.doLogin }>请求接口</Button>
       </div>
     );
   }
 }
 //需要渲染什么数据
-function mapStateToProps(state) {
+const mapStateToProps = (state)=> {
   return {
     tiger: state.count
   }
 }
 //需要触发什么行为
-function mapDispatchToProps(dispatch) {
+const mapDispatchToProps = (dispatch) => {
   return {
     PayIncrease: () => dispatch(setCount(99999)),
   }
 }
 
-export default ReduxTest = connect(mapStateToProps, mapDispatchToProps)(ReduxTest)
+export default Home = connect(mapStateToProps, mapDispatchToProps)(Home)
